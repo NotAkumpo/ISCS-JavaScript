d1b1d3a72bf0599460cf912006dc65c60f42efdc
NotAkumpo <abdiel.evangelista@student.ateneo.edu>
2024-09-22 14:55:42 +0800

    Version 6: Added module 2 practice

---
+++ EventHandling/jstest.js
@@ -0,0 +1,62 @@
+let button = document.querySelector('#button')
+
+
+function networkRequest(url, callBack){
+    
+    let xhr = new XMLHttpRequest()
+
+    xhr.addEventListener('load', callBack)
+
+    xhr.open('GET', 
+        url)
+    xhr.send()
+}
+
+// function buttonClickHandler(evt){   
+//     // console.log(evt)
+//     let xhr = new XMLHttpRequest()
+
+//     xhr.addEventListener('load', evt => {
+//         console.log(xhr.responseText)
+//     })
+
+//     xhr.open('GET', 
+//         'https://jsonplaceholder.typicode.com/posts')
+//     xhr.send()
+//     }
+
+function buttonClickHandler(evt){
+    networkRequest('https://jsonplaceholder.typicode.com/posts').then(res => {
+        console.log(res)
+        let response = JSON.parse(res)
+        return networkRequest('https://jsonplaceholder.typicode.com/posts/1')
+    }).then(res => {
+        console.log(JSON.parse(res).title)
+    }).catch(err => {
+        console.error(err)
+    })
+}
+
+// function buttonClickHandler(evt){
+//     networkRequest('https://jsonplaceholder.typicode.com/posts', evt => {
+//         console.log(this)
+//     })
+// }
+
+button.addEventListener('click', buttonClickHandler)
+
+// button.addEventListener('mouseover', evt => {
+//     console.log(evt.clientX)
+// })
+
+// button.addEventListener('mousemove', evt => {
+//     console.log(evt.clientX)
+// })
+
+button.addEventListener('click', evt => {
+    console.log('Second Handler')
+})
+
+window.addEventListener('load', evt => {
+    console.log('Window Loaded')
+})
